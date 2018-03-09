fe0cc1dc3df3299e4714eb9fc2cd790b69deabce
Andrew McDowell <andrew@propelleraero.com>
2018-03-09 12:17:03 +1100

    new hot module reloading implementation

--- support/demo/main.jsx
+++ support/demo/main.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import document from 'global/document';
-import { AppContainer } from 'react-hot-loader';
-import Root from '../../src/components/Root';
+import { hot } from 'react-hot-loader';
+import HotRoot from './HotRoot';
 
 const READY_STATE_LOADING = 'loading';
 
 function mount() {
-    render(
-        <AppContainer>
-            <Root />
-        </AppContainer>,
-        document.getElementById('mount'),
-    );
+    render(<HotRoot />, document.getElementById('mount'));
 }
 
 if (document.readyState !== READY_STATE_LOADING) {
     mount();
 } else {
-    document.addEventListener('DOMContentLoaded', mount);
-}
-
-if (module.hot) {
-    module.hot.accept('../../src/components/Root', mount);
+    document.addEventListener('DOMContentLoaded', () => mount());
 }
