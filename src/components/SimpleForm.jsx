79d0c48197d6ccdf19bbf0fd90ffadbaa52986a2
mahsaBayat <mbay5694@uni.sydney.edu.au>
2018-04-28 19:25:59 +1000

    remove linting from prepush

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,7 +1,6 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import styled from 'styled-components';
 import FormField from '../components/FormField';
 import Button from '../components/Button';
 
